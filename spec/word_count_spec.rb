55e07fd9dface647b709d6e100e6e2dbdfff121d
Jeffrey Li <jeffrey.li@cantab.net>
2017-03-24 10:00:32 +0800

    passed the first requirement

--- spec/word_count_spec.rb
+++ spec/word_count_spec.rb
@@ -10,4 +10,10 @@ describe ('String') do
     expect(string_input.word_count(word_input)).to(eq(1))
   end
 
+  it("check how frequently a word appears in a given string input") do
+      string_input="hello hello1. hello2, helloo3, hellooo4, hello5 hello!6!"
+      word_input="hello"
+    expect(string_input.word_count(word_input)).to(eq(7))
+  end
+
 end
