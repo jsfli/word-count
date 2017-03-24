7fc940f5ee6ab55949b0f384c198ff130167a3a9
Jeffrey Li <jeffrey.li@cantab.net>
2017-03-24 10:38:49 +0800

    passed test for partial word count function

--- lib/word_count.rb
+++ lib/word_count.rb
@@ -1,9 +1,33 @@
 require('pry')
 
 class String
+
   define_method(:word_count) do |word_input|
+
+    word_count=0
     string_input = self
-    word_count_array=string_input.split(word_input)
+    #clean out all the other characters except alphabets and space
+    string_input_cleaned = string_input.downcase().delete! "^A-Za-z "
+    string_input_array = string_input_cleaned.split(" ")
+
+    string_input_array.each do |element|
+        word_count+=1 if word_input == element
+      end
+
+    return word_count
+  end
+
+
+  define_method(:word_count_partial) do |word_input|
+    string_input = self
+
+    #split by word_input and push it into a new array and
+
+    word_count_array = string_input.split(word_input)
+
+    #count and return the lenth of the array
+
     return word_count_array.length() - 1
+
   end #end of word_count method
 end #end of String
--- spec/word_count_spec.rb
+++ spec/word_count_spec.rb
@@ -5,15 +5,33 @@ require('pry')
 describe ('String') do
 
   it("check how frequently a word appears in a given string input") do
-      string_input="hello! string input"
+      string_input="hello! Hello? hello string input"
+      word_input="hello"
+    expect(string_input.word_count(word_input)).to(eq(3))
+  end
+
+  it("check how frequently a word appears in a given string input") do
+      string_input="hello!"
       word_input="hello"
     expect(string_input.word_count(word_input)).to(eq(1))
   end
 
-  it("check how frequently a word appears in a given string input") do
+  it("check how frequently a word or partial word appears in a given string input") do
+      string_input="hello! hello string input"
+      word_input="hello"
+    expect(string_input.word_count_partial(word_input)).to(eq(2))
+  end
+
+  it("check how frequently a word or partial word appears in a given string input") do
       string_input="hello hello1. hello2, helloo3, hellooo4, hello5 hello!6!"
       word_input="hello"
-    expect(string_input.word_count(word_input)).to(eq(7))
+    expect(string_input.word_count_partial(word_input)).to(eq(7))
+  end
+
+  it("check how frequently a word or partial word appears in a given string input") do
+      string_input="word1 word2 word3 word word5 wor6d"
+      word_input="word"
+    expect(string_input.word_count_partial(word_input)).to(eq(5))
   end
 
 end
